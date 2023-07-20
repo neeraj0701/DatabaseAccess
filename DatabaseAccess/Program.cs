b1318c535a491e4ec2a2e377a2718cd891d5d758
neeraj <neeraj.potlapalli@cognine.com>
2023-07-20 19:09:42 +0530

    Add project files.

---
+++ DatabaseAccess/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace DataAccess
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string conString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            SqlConnection conn = new SqlConnection(conString);
+            //SqlCommand cmd = new SqlCommand("insert into new values(4,'ganesh');", conn);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("select * from new",conn);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            //conn.Open();
+            //Console.WriteLine("Connection Etablished");
+            //int a = cmd.ExecuteNonQuery();
+            //Console.WriteLine(a);
+        }
+    }
+}
+
+
